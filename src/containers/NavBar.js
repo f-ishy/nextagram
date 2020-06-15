d5467c82bb7b74e935a769866f8625eef5e28a97
Ian <fishy90@gmail.com>
2020-06-15 14:49:08 +0800

    fix collapsed navbar not being able to open

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -17,7 +17,7 @@ import { UserContext } from '../contexts/UserContext';
 const NavBar = () => {
     const [currentModal, setCurrentModal] = useState('')
     const [isOpen, setIsOpen] = useState(false);
-    const {currentUser, setCurrentUser} = useContext(UserContext)
+    const { currentUser, setCurrentUser } = useContext(UserContext)
     const history = useHistory();
 
     const logout = () => {
@@ -34,25 +34,25 @@ const NavBar = () => {
         <>
             <Navbar className="mb-3" color="light" light expand="md">
                 <Link to="/"><img src={logo} width='40px' style={{ marginLeft: '15px', marginRight: '15px' }} alt=""></img></Link>
-                <NavbarToggler onClick={(opened) => setIsOpen(!opened)} />
+                <NavbarToggler onClick={() => setIsOpen(opened => !opened)} />
                 <Collapse isOpen={isOpen} navbar>
-                    <Nav className="ml-auto" navbar style={{ alignItems: 'center' }}>
-                        {currentUser !== null //if JWT exists, then remove login/signup buttons
-                            ?
-                            <>
-                                <NavItem>
-                                    <Link to="/profile" style={{ margin: '20px' }}><img src={currentUser.profile_picture} width='30px' height='30px' alt="" /></Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Button color="danger" onClick={logout} style={{ margin: '10px' }}>Logout</Button>
-                                </NavItem>
-                            </>
-                            :
+                    {currentUser !== null //if JWT exists, then remove login/signup buttons
+                        ?
+                        <Nav className="ml-auto" navbar style={{ alignItems: 'center' }}>
+                            <NavItem>
+                                <Link to="/profile" style={{ margin: '20px' }}><img src={currentUser.profile_picture} width='30px' height='30px' alt="" /></Link>
+                            </NavItem>
+                            <NavItem>
+                                <Button color="danger" onClick={logout} style={{ margin: '10px' }}>Logout</Button>
+                            </NavItem>
+                        </Nav>
+                        :
+                        <Nav className="ml-auto" navbar style={{ alignItems: 'center' }}>
                             <NavItem>
                                 <Button color="success" onClick={() => setCurrentModal('login')} style={{ margin: '10px' }}>Login</Button>
                             </NavItem>
-                        }
-                    </Nav>
+                        </Nav>
+                    }
                 </Collapse>
             </Navbar>
             <LoginModal currentModal={currentModal} setCurrentModal={setCurrentModal} />
